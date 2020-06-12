166684b657992b71a4d1e7a425111da01726525f
Vaseekaran-V <vvasee1996@gmail.com>
2020-06-12 23:45:30 +0530

    Making more updates

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,26 @@
-function get_num_of_files{
-	total_files=(*)
-	total_files=$(#total_files[@]}
-	echo $total_files
+#!/usr/bin/env bash
 
-break = 0
+function get_num_of_files {
+	num_of_files=$(ls -l | wc -l)-1
+}
+
+break=0
 
 while [[ $break -eq 0 ]]
 do
-	echo "Please enter the total files in the current directory: "
-	read answer
-	echo "User has input: $answer"
-	$num_of_files = $(get_num_of_files)
-	echo "Total num of files is: $num_of_files"
+  echo "Please enter the total files in the current directory: "
+  read answer
+  echo "User has input: $answer"
+  get_num_of_files
 
-	if [[ answer -eq num_of_files ]]
-	then
-		echo "The answer is correct"
-		break=1
-	elif  [[ num_of_files -gt answer ]]
-		echo "The guessed value is low"
-	else
-		echo "Guessed value is high"
-	fi
+  if [[ answer -eq num_of_files ]]
+  then
+    echo "The answer is correct"
+    break=1
+  elif  [[ num_of_files -gt answer ]]
+  then
+    echo "The guessed value is low"
+  else
+    echo "Guessed value is high"
+  fi
 done
